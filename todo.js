881cc700d90f4370431b5f1804234ac174a26e55
Yoon Hojin <ycool37@naver.com>
2020-03-04 19:52:39 +0900

    배경화면 수정, 이모지 추가

--- todo.js
+++ todo.js
@@ -22,7 +22,7 @@ function saveToDos(){
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "X";
+    delBtn.innerText = "❌";
     delBtn.addEventListener("click",deleteToDo);
     const span = document.createElement("span");
     const newId = toDos.length+1;
